cbbf349918c965b19a8f992a110f0a4f8f31a475
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-14 09:14:02 +0200

    Feature: implementation of axios get and componentdidmount method

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const smurfAPI = 'http://localhost:3333/smurfs';
 
 class App extends Component {
   constructor(props) {
@@ -10,6 +12,13 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
+  }
+  fetchSmurfs = () => {
+    axios.get(smurfAPI).then(res => this.setState({smurfs: res.data}))
+  };
+
+  componentDidMount = () => {
+    this.fetchSmurfs();
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
