853d97bb33176959049877b998e1deaa119217b0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-14 09:45:14 +0200

    Feature: implementation of new form for update with handleChange method

--- village/src/App.js
+++ village/src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      name: '',
+      age: '',
+      height: '',
     };
   }
   fetchSmurfs = () => {
@@ -33,7 +36,11 @@ class App extends Component {
     axios.delete(`${smurfAPI}/${id}`).then(res => this.setState({
       smurfs: res.data
     }))
-  }
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -44,6 +51,28 @@ class App extends Component {
         <NavLink to="/smurf_form" >Smurf Form</NavLink>
         <Route exact path="/" render={() => <Smurfs smurfs={this.state.smurfs} deleter={this.deleteSmurf} />} />
         <Route path="/smurf_form" render={() => <SmurfForm addNewSmurf={this.addSmurf} />} />
+        <form>
+          <input
+            onChange={this.handleInputChange}
+            placeholder="name"
+            value={this.state.name}
+            name="name"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="age"
+            value={this.state.age}
+            name="age"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="height"
+            value={this.state.height}
+            name="height"
+          />
+          <button type="submit">Update</button>
+        </form>
+      
       </div>
     );
   }
